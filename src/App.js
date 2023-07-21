50707b91d85e93be2d0cd272ad6883fb3f053348
박준영 <pjy93525@gmail.com>
2023-07-21 19:33:17 +0900

    Feat: 세팅완료

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/singup" element={<div>회원가입페이지</div>} />
+        <Route path="/login" element={<div>로그인페이지</div>} />
+        <Route path="/myPage" element={<div>마이페이지</div>} />
+        <Route path="/userInfo/:id" element={<div>회원정보페이지</div>} />
+
+        <Route path="/mainPage" element={<div>메인페이지</div>} />
+        <Route path="/articlePage/:id" element={<div>게시글상세페이지</div>} />
+        <Route path="/articlePost/:id" element={<div>게시글 작성페이지</div>} />
+      </Routes>
     </div>
   );
 }
