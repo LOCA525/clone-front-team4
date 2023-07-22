158b7eea309e159c62043df6270828078ba4ba2a
DESKTOP-8GS2VKA\ownage <kjw2878@gmail.com>
2023-07-22 14:18:29 +0900

    feat: Navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Layout from "./pages/common/Layout";
 
 function App() {
   return (
@@ -8,14 +9,15 @@ function App() {
       <Routes>
         <Route path="/singup" element={<div>회원가입페이지</div>} />
         <Route path="/login" element={<div>로그인페이지</div>} />
-        <Route path="/myPage" element={<div>마이페이지</div>} />
-        <Route path="/userInfo/:id" element={<div>회원정보페이지</div>} />
-
-        <Route path="/mainPage" element={<div>메인페이지</div>} />
-        <Route path="/articlePage/:id" element={<div>게시글상세페이지</div>} />
-        <Route path="/articlePost/:id" element={<div>게시글 작성페이지</div>} />
+        <Route element={<Layout />}>
+          <Route path="/myPage" element={<div>마이페이지</div>} />
+          <Route path="/userInfo/:id" element={<div>회원정보페이지</div>} />
+          <Route path="/mainPage" element={<div>메인페이지</div>} />
+          <Route path="/articlePage/:id" element={<div>게시글상세페이지</div>} />
+          <Route path="/articlePost/:id" element={<div>게시글 작성페이지</div>} />
+        </Route>
       </Routes>
-    </div>
+    </div >
   );
 }
 
