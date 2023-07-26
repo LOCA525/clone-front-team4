a39eb9ef90d0c05f39dbc81f8e48129100cf2f69
Yelin-lin <77089775+Yelin-lin@users.noreply.github.com>
2023-07-26 19:07:10 +0900

    Fix: 충돌해결

--- src/App.js
+++ src/App.js
@@ -11,28 +11,30 @@ import Layout2 from "./component/common/Layout2";
 import MyEditPage from "./pages/MyEditPage"
 import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
+
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <div className="App">
       <QueryClientProvider client={queryClient}>
-      <Routes>
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route element={<Layout />}>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/articlePage/:id" element={<Detail />} />
-          <Route path="/editor/:id" element={<div>게시글 수정페이지</div>} />
-        </Route>
-        <Route element={<Layout2 />}>
-          <Route path="/userInfo/:id" element={<MyPage />} />
-          <Route path="/userInfo/:id/Edit" element={<MyEditPage />} />
-          <Route path="/userInfo/:id" element={<MyPage />} />
-        </Route>
-      <Route path="/editor/new" element={<Editor />} />
-    </Routes>
-    </QueryClientProvider>
+        <Routes>
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/articlePage/:id" element={<Detail />} />
+            <Route path="/editor/:id" element={<div>게시글 수정페이지</div>} />
+          </Route>
+          <Route element={<Layout2 />}>
+            <Route path="/userInfo/:id" element={<MyPage />} />
+            <Route path="/userInfo/:id/Edit" element={<MyEditPage />} />
+            <Route path="/userInfo/:id" element={<MyPage />} />
+          </Route>
+          <Route path="/editor/new" element={<Editor />} />
+        </Routes>
+      </QueryClientProvider>
     </div >
   );
 }
