4b0f9590af3a69decb0e9b7fda6a5ea9a2493d5d
Yelin-lin <77089775+Yelin-lin@users.noreply.github.com>
2023-07-26 12:20:45 +0900

    Fix: 마이페이지, 다른회원정보 구분 오류 해결

--- src/component/myPage/Profile.jsx
+++ src/component/myPage/Profile.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
 import LeftProfileBox from './LeftProfileBox'
 import { styled } from 'styled-components'
 import RightPostBox from './RightPostBox'
+import { useParams } from 'react-router-dom';
 
 function Profile({$correctId}) {
+  const { id } = useParams();
+  const [correctId, setCorrectId] = useState(false);
+
+  useEffect(() => {
+    const logInUserString = localStorage.getItem('logInUser');
+    const logInUser = JSON.parse(logInUserString);
+    const logInNickName = logInUser?.nickname;
+    setCorrectId(logInNickName === id);
+  }, [id]);
+
   return (
     <StProfileContainer>
-      <LeftProfileBox $correctId={$correctId}/>
+      <LeftProfileBox $correctId={ correctId }/>
       <div>
         <RightPostBox $pictureName="사진"/>
-        {$correctId && (<RightPostBox $pictureName="좋아요"/>) } 
+        {correctId && (<RightPostBox $pictureName="좋아요"/>) } 
       </div>
     </StProfileContainer>
   )
